eaf4a93f41134e6af0215c3d278725ecc407357e
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2024-06-01 11:05:10 +1000

    создание контроллера постов

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostsController::class, 'index']);
