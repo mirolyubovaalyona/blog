c69eebca608f1e2f10e5eb9540f02a11aa97a6d2
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2024-06-01 22:22:49 +1000

    пагинация + поиск по постам

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,13 +2,78 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
-    public function index() {
-        $posts  = Post::all();
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if ($request->search) {
+            $posts  = Post::join('users', 'author_id', '=', 'users.id')
+                ->where('title',  'like', '%' . $request->search.'%'  )
+                ->orWhere('descr', 'like', '%' . $request->search. '%'  )
+                ->orWhere('name', 'like', '%' . $request->search.'%'  )
+                ->orderBy('posts.created_at')
+                ->paginate(4);
+            return view('posts.index', compact('posts'));
+        }
+        #обьединяем две таблицы чтобы выводить имя автора, сортируем по дате создания поста
+        #пагинация по 4 поста на странице
+        $posts  = Post::join('users', 'author_id', '=', 'users.id')
+                ->orderBy('posts.created_at')
+                ->paginate(4);
         return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class, 'index']);
+Route::get('post/', [PostsController::class, 'index'])->name('post.index');
+ 
